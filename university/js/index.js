2d8550a631cbfbc023c87368d863d50bbcf2d746
Ahmed <59183201+Ahmed-221B@users.noreply.github.com>
2021-05-24 18:58:09 +0200

    Add files via upload

---
+++ university/js/index.js
@@ -0,0 +1,13 @@
+
+var nav_links = document.getElementById("nav_links");
+
+function showMenu(){
+    nav_links.style.right = "0";
+    nav_links.style.display = "block";
+}
+ 
+function hideMenu(){
+    nav_links.style.right = "-200px";
+    nav_links.style.display = "none";
+
+}
